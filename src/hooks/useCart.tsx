8ad111fa1bd29573a1c04238dc9a5e1235cea695
Rafael Vansan <rafaelvansan@MacBook-Air-de-Rafael.local>
2021-05-20 18:38:31 -0300

    page Home

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -35,18 +35,14 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     try {
       const updatedCart = [...cart];
       const productExists = updatedCart.find(product => product.id === productId);
-
       const stock = await api.get(`/stock/${productId}`);
       const stockAmount = stock.data.amount;
-
       const currentAmount = productExists ? productExists.amount : 0;
       const amount = currentAmount + 1;
-
       if(amount > stockAmount){
         toast.error('Quantidade solicitada fora de estoque');
         return;
       }
-      
       if(productExists){
         productExists.amount = amount;
       }
@@ -57,12 +53,9 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
           amount: 1
         }
         updatedCart.push(newProduct);
-
       }
-
       setCart(updatedCart);
       localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
-
     } catch {
       toast.error('Erro na adição do produto');
     }
@@ -71,8 +64,7 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const removeProduct = (productId: number) => {
     try {
       const updatedCart = [...cart];
-      const productIndex = updatedCart.findIndex(product => product.id === productId);
-      
+      const productIndex = updatedCart.findIndex(product => product.id === productId);   
       if(productIndex >= 0){
         updatedCart.splice(productIndex, 1);
         setCart(updatedCart);
@@ -94,18 +86,14 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
       if(amount <= 0){
         return;
       }
-
       const stock = await api.get(`/stock/${productId}`);
       const stockAmount = stock.data.amount;
-
       if(amount > stockAmount){
         toast.error('Quantidade solicitada fora de estoque');
         return;
       }
-
       const updatedCart = [...cart];
       const productExists = updatedCart.find(product => product.id === productId);
-
       if(productExists){
         productExists.amount = amount;
         setCart(updatedCart);
